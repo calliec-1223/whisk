007932d02082109e3e91b3a3d569dd726e0560f7
cmc014 <cmc014@ucsd.edu>
2026-04-06 17:13:41 -0700

    Set up SQLite database and Recipe model

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,22 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///whisk.db'
+db = SQLAlchemy (app)
+
+class Recipe (db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    ingredients = db.Column(db.Text, nullable = False)
+    steps = db.Column(db.Text, nullable = False)
+    notes = db.Column(db.String(200))
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/ping')
 def ping():
