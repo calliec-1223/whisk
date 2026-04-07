ad261a1549001d82317d064f6fe35f0b39ba7b93
cmc014 <cmc014@ucsd.edu>
2026-04-06 18:07:24 -0700

    add recipe form

---
+++ frontend/src/AddRecipe.js
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+
+function AddRecipe({ onRecipeAdded }) {
+    const [form, setForm] = useState({
+        title: '',
+        ingredients: '',
+        steps: '',
+        notes: ''
+    })
+
+    const handleChange = (e) => {
+        setForm({...form, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = () => {
+        fetch('http://127.0.0.1:5000/recipes', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify(form)
+        })
+        .then(res => res.json())
+        .then (() => {
+            onRecipeAdded()
+            setForm ({title: '', ingredients: '', steps: '', notes: ''})
+        })
+    }
+    
+    return (
+        <div>
+            <h2>Add a Recipe</h2>
+            <input
+                name = "title"
+                placeholder = "Recipe Title"
+                value = {form.title}
+                onChange = {handleChange}
+            />
+            <textarea
+                name = "ingredients"
+                placeholder = "Ingredients (one per line)"
+                value = {form.ingredients}
+                onChange = {handleChange}
+            />
+            <textarea
+                name = "steps"
+                placeholder = "Steps"
+                value = {form.steps}
+                onChange = {handleChange}
+            />
+            <input
+                name = "notes"
+                placeholder = "Notes (optional)"
+                value = {form.notes}
+                onChange = {handleChange}
+            />
+            <button onClick = {handleSubmit} > Save Recipe</button>
+        </div>
+    )
+}
+
+export default AddRecipe
+
+
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,32 @@
 import { useEffect, useState } from 'react'
+import AddRecipe from './AddRecipe'
 
 function App() {
-  const [status, setStatus] = useState('')
+  const [recipes, setRecipes] = useState([])
+
+  const fetchRecipes = () => {
+    fetch('http://127.0.0.1:5000/recipes')
+      .then(res => res.json())
+      .then (data => setRecipes(data))
+  }
 
   useEffect(() => {
-    fetch('http://127.0.0.1:5000/ping')
-      .then(res => res.json())
-      .then(data => setStatus(data.message))
+    fetchRecipes()
   }, [])
 
   return (
     <div>
       <h1>Whisk 🧁</h1>
-      <p>{status}</p>
+      <AddRecipe onRecipeAdded = {fetchRecipes} />
+      {recipes.map(recipe => (
+        <div key= {recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.ingredients}</p>
+          <p>{recipe.steps}</p>
+          <p>{recipe.notes}</p>
+          <hr/>
+          </div>
+      ))}
     </div>
   )
 }
